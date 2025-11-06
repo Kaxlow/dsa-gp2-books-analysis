b4b89b33764e835580abcf85d0c014123c6c5094
klow <kenxhlow@gmail.com>
2025-11-05 22:28:10 -0700

    fix: Added docstring to text preprocessing function.

--- src/textpreprocessing.py
+++ src/textpreprocessing.py
@@ -2,34 +2,44 @@ from pathlib import Path
 import re
 import nltk
 
-# Define text preprocessing function which takes a text file and returns a list of tokenized words
 def txt_preprocessing(txt_file = "PrideAndPrejudice.txt"):
 
-    # Read all the text from Pride and Prejudice into a str var
-    current_path = Path(__file__)
-    input_path = current_path.parent.parent.resolve() / txt_file
-    with input_path.open(mode = "r", encoding = "UTF-8") as file:
-      txt = file.read()
+  '''
+  This function takes a .txt file and returns a list of tokenized words by performing the following steps:
+  1. Read the contents of a .txt file into a string variable. By defaults, it reads the text from the book "Pride and Prejudice".
+  2. Converts all letters into lowercase.
+  3. Removes all punctuation, digits, and special characters, while keeping apostrophes.
+  4. Tokenizes the text into a ist of words by separating on whitespace.
+  5. Removes common English stopwords from the list of tokens, using ntlk.corpus's stopwords list as reference.
 
-    # Convert all text to lowercase
-    txt = txt.lower()
+  The function also prints a summary showing the number of tokens before and after removing the stopwords.
+  '''
 
-    # Remove punctuation, digits, and special characters. Keep apostrophes.
-    txt = re.sub(r"[^a-z\s\']", "", txt)
+  # Read text from .txt file into a string
+  current_path = Path(__file__)
+  input_path = current_path.parent.parent.resolve() / txt_file
+  with input_path.open(mode = "r", encoding = "UTF-8") as file:
+    txt = file.read()
 
-    # Tokenize text into a list of words by separating on whitespace
-    txt = txt.split()
-    # Count number of tokens before removing stopwords
-    tokens_bef = len(txt)
+  # Convert all text to lowercase
+  txt = txt.lower()
 
-    # Remove stopwords from list of tokens
-    from nltk.corpus import stopwords
-    en_stopwords = stopwords.words("english")
-    txt = [word for word in txt if word not in en_stopwords]
-    # Count number of tokens after removing stopwords
-    tokens_aft = len(txt)
+  # Remove punctuation, digits, and special characters. Keep apostrophes.
+  txt = re.sub(r"[^a-z\s\']", "", txt)
 
-    # Token count summary
-    print(f"Before removing stopwords, there were {tokens_bef} tokens.\nAfter removing the stopwords, there were {tokens_aft} tokens.")
+  # Tokenize text into a list of words by separating on whitespace
+  txt = txt.split()
+  # Count number of tokens before removing stopwords
+  tokens_bef = len(txt)
 
-    return txt
+  # Remove stopwords from list of tokens
+  from nltk.corpus import stopwords
+  en_stopwords = stopwords.words("english")
+  txt = [word for word in txt if word not in en_stopwords]
+  # Count number of tokens after removing stopwords
+  tokens_aft = len(txt)
+
+  # Token count summary
+  print(f"Before removing stopwords, there were {tokens_bef} tokens.\nAfter removing the stopwords, there were {tokens_aft} tokens.")
+
+  return txt
